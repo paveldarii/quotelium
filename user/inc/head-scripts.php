a6012c0bb6dca68962e2348a63149a42d387042a
Pavel Darii <paveldarii@yahoo.com>
2021-07-18 23:00:48 -0400

    minified bootstrap styles with all customization I made, then created styles.css that will be used for other style add on

--- user/inc/head-scripts.php
+++ user/inc/head-scripts.php
@@ -1,4 +1,5 @@
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="/_css/styles.min.css">
 <link rel="stylesheet" href="/_css/styles.css">
 <title>PHP Learning</title>
